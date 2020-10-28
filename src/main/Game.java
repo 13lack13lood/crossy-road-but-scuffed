12efe1c037d879de08f8bd0a0299035611ddb526
13lack13lood <44007891+13lack13lood@users.noreply.github.com>
2020-10-27 22:23:34 -0400

    put jFrame to seperate file

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,12 +1,16 @@
 package main;
 
+import java.awt.Container;
 import java.awt.Dimension;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Game extends JFrame{
 	
 	private Dimension screenDimension;
+	private ImageIcon icon;
+	private Container container;
 	
 	public Game(int WIDTH, int HEIGHT) {
 		this.screenDimension = new Dimension(WIDTH, HEIGHT);
@@ -14,5 +18,14 @@ public class Game extends JFrame{
 		this.setResizable(false);
 		this.setSize(screenDimension);
 		this.setVisible(true);
+		
+		container = this.getContentPane();
+		
+		icon = new ImageIcon("res/marioicon.png");
+		this.setIconImage(icon.getImage());
+	}
+	
+	public Container getContainer() {
+		return container;
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,11 @@ package main;
 
 public class Main {
 
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
+	
 	public static void main(String[] args) {
-		
+		Game frame = new Game(WIDTH, HEIGHT);
 	}
 
 }
