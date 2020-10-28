c2ff1daadd649537b41804f4e149cba6be4218d7
Justin Lee <56641610+thiccpenguin@users.noreply.github.com>
2020-10-27 22:06:52 -0400

    pog

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,11 @@
 package main;
 
+import menu.StarterMenu;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		StarterMenu startermenu = new StarterMenu();
 	}
 
 }
--- src/menu/StarterMenu.java
+++ src/menu/StarterMenu.java
@@ -6,15 +6,15 @@ import javax.swing.border.Border;
 
 public class StarterMenu {
 	
-	StarterMenu(){
+	public StarterMenu(){
 		ImageIcon icon= new ImageIcon("res/marioicon.png");
-		ImageIcon back= new ImageIcon("res/mariobackground.jpg");
-		
+		ImageIcon back= new ImageIcon("res/mario_background.png");
 		
 		Border border = BorderFactory.createLineBorder(Color.black, 50);
 		JLabel label = new JLabel();
 		label.setIcon(back);
-		label.setText("POGGERS");
+		JLabel label2 = new JLabel();
+		label2.setIcon(back);
 		
 		JPanel panel1 = new JPanel();
 		panel1.setBackground(Color.BLUE);
@@ -28,14 +28,16 @@ public class StarterMenu {
 		Container container = frame.getContentPane();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(1600,800);
-		frame.setLayout(null);
-		frame.setVisible(true);
-		frame.setIconImage(icon.getImage());
 		
+		frame.setLayout(null);
+		frame.setLayout(new FlowLayout());
+		frame.setIconImage(icon.getImage());
 		container.setBackground(Color.RED);
-		panel1.setBorder(border);
-		panel1.add(label);
-		frame.add(panel1);
-		frame.add(panel2);
+		frame.add(label);
+		frame.add(label2);
+	
+		
+		frame.setVisible(true);
+		frame.setResizable(false);
 	}
 }
