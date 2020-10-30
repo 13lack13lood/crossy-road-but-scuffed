ce1a45d1f1b62daeb05116e99c12f4f094aad471
Justin Lee <56641610+thiccpenguin@users.noreply.github.com>
2020-10-30 19:25:46 -0400

    I coded the title/image onto the main menu and formatted the text

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,9 @@
 package main;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
 
 import game.gamePanel.GamePanel;
 import menus.StartMenu;
@@ -10,12 +13,11 @@ public class Main {
 	public static void main(String[] args) {
 		Frame frame = new Frame();
 		
-		StartMenu startMenu = new StartMenu();
+		StartMenu startMenu = new StartMenu(new ImageIcon("res/chicken.png"),new Font("Comic Sans MS",Font.BOLD,20));
 		
 		GamePanel gamePanel = new GamePanel();
 
 		Frame.container.add(startMenu, BorderLayout.CENTER);
 		Frame.container.add(gamePanel, BorderLayout.CENTER);
-		Frame.layout.addLayoutComponent("startmenu", startMenu);
 	}
 }
--- src/menus/StartMenu.java
+++ src/menus/StartMenu.java
@@ -5,41 +5,58 @@ import javax.swing.JPanel;
 import main.Frame;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class StartMenu extends JPanel implements ActionListener{
 	
 
 	private JButton play, info, exit;
+	private ImageIcon chicken;
+	private Font font;
+	private JLabel title;
 	
-	public StartMenu() {
+	public StartMenu(ImageIcon chicken, Font font) {
 		
+		this.font = font;
+		this.chicken = chicken;
 		this.setBackground(new Color(105, 206, 236));
 		
+		JLabel title = new JLabel();
+		title.setText("Crossy Road But Scuffed");
+		title.setLocation(20,10);
+		title.setSize(500, 100);
+		title.setFont(new Font("Comic Sans MS",Font.BOLD,38));
+		
 		play = new JButton();
-		play.setBounds(5,350,120,75);
+		play.setBounds(30,350,120,75);
 		play.addActionListener(this);
 		play.setText("PLAY");
-		play.setFont(new Font("Comic Sans",Font.BOLD,20));
+		play.setFont(font);
 		play.setBorder(BorderFactory.createLineBorder(Color.black));
+		play.setBackground(Color.white);
 		
 		info = new JButton();
 		info.setBounds(190,350,120,75);
 		info.addActionListener(this);
 		info.setText("INFO");
-		info.setFont(new Font("Comic Sans",Font.BOLD,20));
+		info.setFont(font);
 		info.setBorder(BorderFactory.createLineBorder(Color.black));
+		info.setBackground(Color.white);
 
 		exit = new JButton();
-		exit.setBounds(370,350,120,75);
+		exit.setBounds(350,350,120,75);
 		exit.addActionListener(this);
 		exit.setText("EXIT");
-		exit.setFont(new Font("Comic Sans",Font.BOLD,20));
+		exit.setFont(font);
 		exit.setBorder(BorderFactory.createLineBorder(Color.black));
+		exit.setBackground(Color.white);
 		
 		this.setLayout(null);
 		this.setVisible(true);
@@ -47,6 +64,8 @@ public class StartMenu extends JPanel implements ActionListener{
 		this.add(play);
 		this.add(info);
 		this.add(exit);
+		this.add(title);
+		repaint();
 	
 	}
 	public void actionPerformed(ActionEvent e) {
@@ -60,4 +79,8 @@ public class StartMenu extends JPanel implements ActionListener{
 			System.exit(0);
 		}
 	}
+	public void paintComponent(Graphics g) {
+          super.paintComponent(g);
+          g.drawImage(chicken.getImage(),120 , 90, 250, 250, null);
+    }
 }
