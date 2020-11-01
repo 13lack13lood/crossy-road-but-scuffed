09e1e50f97556901c79fe28fa6101ba397761e3f
13lack13lood <44007891+13lack13lood@users.noreply.github.com>
2020-11-01 12:59:19 -0500

    edited some stuff

--- src/game/entities/Player.java
+++ src/game/entities/Player.java
@@ -15,14 +15,10 @@ public class Player extends Entity {
 	private int moveDirection;
 
 	public Player(ImageIcon playerIcon, int y, int dy) {
-		super(playerIcon, new HitBox(Frame.SQUARE, y, Frame.SQUARE, Frame.SQUARE), Frame.SQUARE, Frame.SQUARE, y, false);
+		super(playerIcon, new HitBox(Frame.SQUARE, y, Frame.SQUARE, Frame.SQUARE), Frame.SQUARE, Frame.SQUARE * 3, y, false);
 		this.playerIcon = playerIcon;
 		this.y = y;
 		this.dy = dy;
-	}
-	
-	public void draw(Graphics g) {
-		g.drawImage(playerIcon.getImage(), super.getX(), super.gettY(), null);
 	}
 	
 	public int getMoveDirection() {
