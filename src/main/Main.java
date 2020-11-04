f07500a646ba4fa05d2a4bf19d7dda5c9f77203d
unknown <334504347@TDSB.ON.CA>
2020-11-04 09:59:47 -0500

    Revert "created end menu and added graphics"
    This reverts commit 6eec1b6ea77ea84981c5607bf998ca238e979e21.

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,6 @@ import javax.swing.ImageIcon;
 import game.entities.Player;
 import game.gamePanel.GamePanel;
 import game.terrain.TerrainGenerator;
-import menus.EndMenu;
 import menus.InstructionMenu;
 import menus.StartMenu;
 
@@ -18,7 +17,6 @@ public class Main {
 		
 		StartMenu startMenu = new StartMenu(new ImageIcon("res/chicken.png"), new Font("Comic Sans MS", Font.BOLD, 20));
 		InstructionMenu instructionMenu = new InstructionMenu(new ImageIcon("res/W.png"), new ImageIcon("res/S.png"), new ImageIcon("res/D.png"), new Font("Comic Sans MS", Font.BOLD, 25));
-		EndMenu endMenu = new EndMenu(new ImageIcon("res/gameover.png"),new Font("Comic Sans MS", Font.BOLD, 20));
 		
 		Player player = new Player(new ImageIcon("res/chicken.png"), Frame.SQUARE * 3, 8);
 		TerrainGenerator terrainGenerator = new TerrainGenerator(Frame.HEIGHT / Frame.SQUARE);
@@ -27,7 +25,6 @@ public class Main {
 		Frame.container.add(startMenu, "startmenu");
 		Frame.container.add(instructionMenu, "instructionmenu");
 		Frame.container.add(gamePanel, "gamepanel");
-		Frame.container.add(endMenu, "endmenu");
 		
 		frame.setVisible(true);
 	}
